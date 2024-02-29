9d99a97a217f745bf3221bab69151982ebc58602
Nasiim Nuur <nasiimmo.691@gmail.com>
2024-02-29 18:45:50 +0000

    added linkedin link

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer(){
         Made by Nasiim & Husaam &copy; Global Goods {new Date().getFullYear()}
         <div className='socials'>
         <a href='https://github.com/nasiimmo' className="github-link"><img src={Github} /> </a>
-        <a href='https://www.linkedin.com/feed/' className="linkedIn-link"><img src={LinkedIn} /> </a>
+        <a href='https://www.linkedin.com/in/nasiim-nuur-59b31b261/' className="linkedIn-link"><img src={LinkedIn} /> </a>
         </div>
       </footer>
   )
